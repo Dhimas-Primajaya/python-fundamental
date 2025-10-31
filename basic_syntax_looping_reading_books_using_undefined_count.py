e869fdcb48afea007b70d5fd789854a382c91bed
Dhimas Primajaya <131242124+dhimasprimaj@users.noreply.github.com>
2025-11-01 05:09:28 +0700

    shortening variables

--- basic_syntax_looping_reading_books_using_undefined_count.py
+++ basic_syntax_looping_reading_books_using_undefined_count.py
@@ -2,24 +2,24 @@
 Perulangan dengan while samopai paham
 """
 books = 10
-books_read_total = 0
-books_read_and_understood = 0
+books_read = 0
+books_understood = 0
 
 print('Ibu berkata, "Baca dan pahami semua bukumu"\n')
-print(f"Jumlah buku yang sudah dibaca dan dipahami =  {books_read_and_understood}\n")
+print(f"Jumlah buku yang sudah dibaca dan dipahami =  {books_understood}\n")
 
-while books_read_total < books * 2:
-    books_read_total = books_read_total + 1
-    if  books_read_and_understood == 9:
-        print(f"Buku ke {books_read_and_understood + 1} belum paham")
+while books_read < books * 2:
+    books_read = books_read + 1
+    if  books_understood == 9:
+        print(f"Buku ke {books_understood + 1} belum paham")
     else:
-        books_read_and_understood = books_read_and_understood + 1
-        print(f"Buku ke sudah dibaca dan dipahami {books_read_and_understood}")
+        books_understood = books_understood + 1
+        print(f"Buku ke sudah dibaca dan dipahami {books_understood}")
 
-print(f"Jumlah buku yang sudah dibaca dan dipahami = {books_read_and_understood}")
+print(f"Jumlah buku yang sudah dibaca dan dipahami = {books_understood}")
 
-if books_read_and_understood == books:
+if books_understood == books:
     print("Bu, semua buku sudah dibaca dan dipahami")
 else:
     print(f"Bu, tidak semua buku dipahami. "
-          f"Budi hanya bisa memahami {books_read_and_understood} buku")
+          f"Budi hanya bisa memahami {books_understood} buku")
