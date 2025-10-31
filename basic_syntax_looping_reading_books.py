edec6f0faf1c1823b029822e51ba55bd1ab577b8
Dhimas Primajaya <131242124+dhimasprimaj@users.noreply.github.com>
2025-10-31 16:55:37 +0700

    Looping Reading Books

---
+++ basic_syntax_looping_reading_books.py
@@ -0,0 +1,14 @@
+"""
+Program perulangan membaca buku
+"""
+
+books = 110
+print('Ibu berkata, "Baca semua bukumu"\n')
+
+books_readed = 0
+print(f"Jumlah buku yang sudah dibaca {books_readed}\n")
+
+for books_readed in range(1, books+1):
+    print(f"Buku ke {books_readed} sudah dibaca")
+
+print(f"\nJumlah buku yang sudah dibaca {books_readed}")
